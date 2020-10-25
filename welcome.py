f6ca86c84d935569153c0bc0352ae3186fbc15f7
Oppong-Agyare ANOKYE NKANSAH <72044731+oanokyen@users.noreply.github.com>
2020-10-25 23:41:48 +0000

    Update and rename HillsongRepo.py to welcome.py
    I have this bad obsession with caps

--- welcome.py
+++ welcome.py
@@ -3,9 +3,9 @@ name=input("What's your name? ")
 print("Welcome to my Hillsong Repo,",name)
 res=input("You love Hillsongs too? Y/N  ")
 if res=="Y": 
- print("Awesome!")
+    print("Awesome!")
 else: 
- print("Come on??? but don't worry it still works,anyway")
+    print("Come on??? but don't worry it still works,anyway")
 print("A moments as I setup for you")
 import random
 n=0
