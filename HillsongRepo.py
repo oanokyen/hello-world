7cc5a23606211fd2311dcc62638ebf40f06933a6
Oppong-Agyare ANOKYE NKANSAH <72044731+oanokyen@users.noreply.github.com>
2020-10-25 23:38:19 +0000

    Add files via upload
    Uploaded script file. A quick intro and an inhumane conversation. You can download and run.

---
+++ HillsongRepo.py
@@ -0,0 +1,17 @@
+print("Hello There! I am glad you found me!")
+name=input("What's your name? ")
+print("Welcome to my Hillsong Repo,",name)
+res=input("You love Hillsongs too? Y/N  ")
+if res=="Y": 
+ print("Awesome!")
+else: 
+ print("Come on??? but don't worry it still works,anyway")
+print("A moments as I setup for you")
+import random
+n=0
+while n<101:	
+    print(n)
+    z=random.randint(1, 10)
+    n=n+z
+print("Loaded Successfully!")
+print("Lezzz get to it")
